d54700e412c8b3db919e3f341caa1f1a235ca0d6
glendc <glen.decauwsemaecker@otainsight.com>
2021-12-10 22:33:52 +0100

    add wrong and still slow solution for part2 of day6 (2021)

--- 2021/day6/src/main.rs
+++ 2021/day6/src/main.rs
@@ -1,4 +1,7 @@
 fn main() {
+    println!("++++++++++++");
+    println!("PART#1: Ok");
+    println!("++++++++++++");
     part1(TEST_INPUT, 18);
     part1(TEST_INPUT, 80);
 
@@ -7,12 +10,25 @@ fn main() {
     part1(INPUT, 80);
 
     println!("++++++++++++");
+    println!("PART#2: too slow and even wrong");
+    println!("++++++++++++");
 
-    part2(TEST_INPUT, 256);
+    part2_slow_and_wrong(TEST_INPUT, 18);
+    part2_slow_and_wrong(TEST_INPUT, 80);
 
     println!("-----");
 
-    part2(INPUT, 256);
+    part2_slow_and_wrong(INPUT, 80);
+
+    println!("-----");
+
+    part2_slow_and_wrong(TEST_INPUT, 140);
+
+    println!("++++++++++++");
+    println!("PART#2: Ok");
+    println!("++++++++++++");
+
+    part2(TEST_INPUT, 140);
 }
 
 fn part1(input: &str, days: usize) {
@@ -35,6 +51,28 @@ fn part1(input: &str, days: usize) {
         // println!("After {} Day(s): {:?}", i + 1, ages);
     }
     println!("{}", school.len());
+}
+
+fn part2_slow_and_wrong(input: &str, days: usize) {
+    let mut school: Vec<LanternFishSnapshot> = input
+        .split(",")
+        .map(|s| {
+            let counter: u8 = s.parse().unwrap();
+            LanternFishSnapshot::new(Some(counter), 0)
+        })
+        .collect();
+    let mut total_fish_count = school.len();
+    while !school.is_empty() {
+        school = school
+            .into_iter()
+            .map(|snapshot| snapshot.into_snapshots(days))
+            .fold(Vec::new(), |mut school, snapshots| {
+                school.extend(snapshots);
+                school
+            });
+        total_fish_count += school.len();
+    }
+    println!("{}", total_fish_count);
 }
 
 fn part2(_input: &str, _days: usize) {
@@ -70,6 +108,51 @@ impl LanternFish {
     // pub fn counter(&self) -> u8 {
     //     self.counter
     // }
+}
+
+struct LanternFishSnapshot {
+    day: usize,
+    cycle: u8,
+    counter: u8,
+}
+
+impl LanternFishSnapshot {
+    pub fn new(counter: Option<u8>, day: usize) -> LanternFishSnapshot {
+        match counter {
+            None => LanternFishSnapshot {
+                day,
+                cycle: 9,
+                counter: 0,
+            },
+            Some(counter) => LanternFishSnapshot {
+                day,
+                cycle: 7,
+                counter,
+            },
+        }
+    }
+
+    pub fn into_snapshots(mut self, max_day: usize) -> Vec<LanternFishSnapshot> {
+        let mut children = Vec::new();
+        if self.counter > 0 {
+            self.day += (self.counter + 1) as usize;
+            if self.day >= max_day {
+                return children;
+            }
+            children.push(LanternFishSnapshot::new(None, self.day));
+        } else if self.cycle == 9 {
+            self.day += 9;
+            if self.day >= max_day {
+                return children;
+            }
+            children.push(LanternFishSnapshot::new(None, self.day));
+        }
+        // create all remaining
+        for offset in 0..((max_day - self.day) / 7) {
+            children.push(LanternFishSnapshot::new(None, self.day + (offset + 1) * 7));
+        }
+        children
+    }
 }
 
 const TEST_INPUT: &'static str = "3,4,3,1,2";
