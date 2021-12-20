e4b6fc1c11f4dd2da01b2982d50c481352364ba0
glendc <glen.decauwsemaecker@otainsight.com>
2021-12-21 00:07:28 +0100

    add empty day17/day20

---
+++ 2021/day17/src/main.rs
@@ -0,0 +1,31 @@
+fn main() {
+    println!("++++++++++++");
+    println!("PART#1");
+    println!("++++++++++++");
+
+    part1(TEST_INPUT);
+
+    // println!("-----");
+
+    // part1(INPUT);
+
+    // println!("++++++++++++");
+    // println!("PART#2");
+    // println!("++++++++++++");
+
+    // part2(TEST_INPUT);
+
+    // println!("-----");
+
+    // part2(INPUT);
+}
+
+fn part1(input: &str) {
+}
+
+fn part2(input: &str) {
+}
+
+const TEST_INPUT: &'static str = "target area: x=20..30, y=-10..-5";
+
+const INPUT: &'static str = "target area: x=209..238, y=-86..-59";
